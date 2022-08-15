40834cb0fe64cb8fc435b4da4eccb1bd036fdd17
hayounlee-lab <111311328+hayounlee-lab@users.noreply.github.com>
2022-08-15 13:24:53 -0700

    Update runImmunIC.bash

--- ImmunIC/runImmunIC.bash
+++ ImmunIC/runImmunIC.bash
@@ -1,7 +1,12 @@
+#!/bin/bash
+# Bash file to run ImmunIC
+# Usage: bash runImmunIC.bash input_csv_file
+
 inputfile=$1
 if [ ! -f $1 ]; then
    echo $1" not found"
    exit
 fi
+
 python3 /root/ImmunIC/xgboost-cd4cd8Tcells.py $inputfile
 python3 /root/ImmunIC/ImmunIC.py $inputfile
